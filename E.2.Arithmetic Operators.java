2247a7db2c99bfb862d145b4223f92322e0d26c5
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2023-09-26 17:13:32 +0530

    E.2.Arithmetic Operators.java

---
+++ E.2.Arithmetic Operators.java
@@ -0,0 +1,99 @@
+Arithmetic Operators
+Arithmetic operators are used to perform common mathematical operations.
+
+
+Operator	        Name	              Description	                                    Example
+  +	             Addition	          Adds together two values	                         x + y	
+  -	             Subtraction	      Subtracts one value from another	                 x - y	
+  *	             Multiplication	    Multiplies two values	                             x * y	
+  /	             Division	          Divides one value by another	                     x / y	
+  %  	           Modulus	          Returns the division remainder	                   x % y	
+  ++	           Increment          Increases the value of a variable by 1	            ++x	
+  --	           Decrement	        Decreases the value of a variable by 1	            --x
+
+        ADDITION    
+public class Main {
+public static void main(String[] args) {
+int x = 5;
+int y = 3;
+System.out.println(x + y);
+}
+}
+/*
+    8
+*/
+
+      SUBTRACTION
+public class Main {
+public static void main(String[] args) {
+int x = 5;
+int y = 3;
+System.out.println(x - y);
+}
+}
+/*
+    2
+*/
+
+      MULTIPLICATION
+public class Main {
+public static void main(String[] args) {
+int x = 5;
+int y = 3;
+System.out.println(x * y);
+}
+}
+/*
+    15
+*/
+
+        DIVISION
+public class Main {
+public static void main(String[] args) {
+int x = 5;
+int y = 3;
+System.out.println(x / y);
+}
+}
+/*
+    1
+*/
+
+        Modulus
+public class Main {
+public static void main(String[] args) {
+int x = 5;
+int y = 2;
+System.out.println(x % y);
+}
+}
+/*
+ 1
+*/
+
+        INCREMENT
+public class Main {
+public static void main(String[] args) {
+int x = 5;
+++x;
+System.out.println(x);
+}
+}
+/*
+    6
+*/
+          DECREMENT
+public class Main {
+public static void main(String[] args) {
+int x = 5;
+--x;
+System.out.println(x);
+}
+}
+/*
+    4
+*/
+           
+
+
+        
