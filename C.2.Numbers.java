0db785fc6f6a0d68822a8b007f547bdeb8f902d7
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2023-09-26 13:50:12 +0530

    C.2.Numbers.java

--- C.2.Numbers.java
+++ C.2.Numbers.java
@@ -91,4 +91,22 @@ System.out.println(num);
 /*
     19.99
 */
-  
+
+Use float or double?
+
+The precision of a floating point value indicates how many digits the value can have after the decimal point. 
+The precision of float is only six or seven decimal digits, while double variables have a precision of about 15 digits. 
+Therefore it is safer to use double for most calculations.
+import java.io.*;
+public class Main {
+public static void main(String[] args) {
+float f1 = 35e3f;
+double d1 = 12E4d;
+System.out.println(f1);
+System.out.println(d1);  
+}
+}
+/*
+    35000.0
+    120000.0
+*/
