a990f0f3189cbd9c7b8ca548ba169ce60d7e3cda
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2023-09-26 15:53:12 +0530

    C.3.Booleans.java

---
+++ C.3.Booleans.java
@@ -0,0 +1,19 @@
+Very often in programming, you will need a data type that can only have one of two values, like:
+
+YES / NO
+ON / OFF
+TRUE / FALSE
+
+import java.io.*;
+public class Main {
+public static void main(String[] args) {
+boolean isprimitivedatatype = true;
+boolean isnonprimitivedatatype = false;    
+System.out.println(isprimitivedatatype);
+System.out.println(isnonprimitivedatatype);
+}
+}
+/*
+true
+false
+*/
